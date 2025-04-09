c408e8756b2196da1ba4ada1f4afecb136763325
Prashant Saw <prashantsaw19@gmail.com>
2025-04-09 21:20:57 +0530

    feat: updated mappers

--- src/main/java/in/oxidane/work/done/order/mapper/WorkOrderLineItemsMapper.java
+++ src/main/java/in/oxidane/work/done/order/mapper/WorkOrderLineItemsMapper.java
@@ -1,20 +1,17 @@
 package in.oxidane.work.done.order.mapper;
 
 import in.oxidane.work.done.common.config.MapstructMapperConfig;
-import in.oxidane.work.done.lineitem.entity.LineItem;
-import in.oxidane.work.done.lineitem.mapper.LineItemMapper;
+import in.oxidane.work.done.common.mapper.IdToEntityMapper;
 import in.oxidane.work.done.order.dto.WorkOrderLineItemsRequest;
 import in.oxidane.work.done.order.dto.WorkOrderLineItemsResponse;
-import in.oxidane.work.done.order.entity.WorkOrder;
 import in.oxidane.work.done.order.entity.WorkOrderLineItems;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 
 import java.util.List;
 
-@Mapper(config = MapstructMapperConfig.class, uses = {WorkOrderMapper.class, LineItemMapper.class})
+@Mapper(config = MapstructMapperConfig.class, uses = {IdToEntityMapper.class})
 public interface WorkOrderLineItemsMapper {
 
     @Mapping(target = "workOrder", source = "workOrderId", qualifiedByName = "toWorkOrder")
@@ -28,24 +25,4 @@ public interface WorkOrderLineItemsMapper {
     @Mapping(target = "workOrder", source = "workOrderId", qualifiedByName = "toWorkOrder")
     @Mapping(target = "woAllocatedLineItem", source = "woAllocatedLineItemId", qualifiedByName = "toLineItem")
     WorkOrderLineItems toUpdateEntityFromRequest(WorkOrderLineItemsRequest request, @MappingTarget WorkOrderLineItems entity);
-
-    @Named("toWorkOrder")
-    default WorkOrder toWorkOrder(Long workOrderId) {
-        if (workOrderId == null) {
-            return null;
-        }
-        return WorkOrder.builder()
-            .workOrderId(workOrderId)
-            .build();
-    }
-
-    @Named("toLineItem")
-    default LineItem toLineItem(Long lineItemId) {
-        if (lineItemId == null) {
-            return null;
-        }
-        return LineItem.builder()
-            .lineItemId(lineItemId)
-            .build();
-    }
 }
--- src/main/java/in/oxidane/work/done/project/mapper/ProjectMapper.java
+++ src/main/java/in/oxidane/work/done/project/mapper/ProjectMapper.java
@@ -1,42 +1,28 @@
 package in.oxidane.work.done.project.mapper;
 
 import in.oxidane.work.done.common.config.MapstructMapperConfig;
+import in.oxidane.work.done.common.mapper.IdToEntityMapper;
 import in.oxidane.work.done.project.dto.ProjectRequest;
 import in.oxidane.work.done.project.dto.ProjectResponse;
-import in.oxidane.work.done.project.entity.Client;
 import in.oxidane.work.done.project.entity.Project;
-import in.oxidane.work.done.project.entity.ProjectStatus;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 
 import java.util.List;
 
-@Mapper(config = MapstructMapperConfig.class)
+@Mapper(config = MapstructMapperConfig.class, uses = {IdToEntityMapper.class})
 public interface ProjectMapper {
 
     ProjectResponse toResponse(Project entity);
 
-    @Mapping(target = "projectId", ignore = true)
-    @Mapping(target = "client", expression = "java(createClient(request.getClientId()))")
-    @Mapping(target = "projectStatus", expression = "java(createProjectStatus(request.getProjectStatusId()))")
+    @Mapping(target = "client", source = "clientId", qualifiedByName = "toClient")
+    @Mapping(target = "projectStatus", source = "projectStatusId", qualifiedByName = "toProjectStatus")
     Project toEntity(ProjectRequest request);
 
     List<ProjectResponse> toResponse(List<Project> projects);
 
-    @Mapping(target = "projectId", ignore = true)
-    @Mapping(target = "client", expression = "java(createClient(request.getClientId()))")
-    @Mapping(target = "projectStatus", expression = "java(createProjectStatus(request.getProjectStatusId()))")
+    @Mapping(target = "client", source = "clientId", qualifiedByName = "toClient")
+    @Mapping(target = "projectStatus", source = "projectStatusId", qualifiedByName = "toProjectStatus")
     Project toUpdateEntityFromRequest(ProjectRequest request, @MappingTarget Project entity);
-
-    @Named("createClient")
-    default Client createClient(Long id){
-        return Client.builder().clientId(id).build();
-    }
-
-    @Named("createProjectStatus")
-    default ProjectStatus createProjectStatus(Long id){
-        return ProjectStatus.builder().projectStatusId(id).build();
-    }
 }
